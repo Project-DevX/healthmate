a49fa8a848c86178e43dcc920f4cff44250bc879
Himeth14092001 <en108909@foe.sjp.ac.lk>
2025-06-22 14:00:58 +0530

    sha1 fingerprint adjustment

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.devx.healthmate"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"  // Use this consistent version
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
